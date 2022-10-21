6123858beca699500738a0fd527621ffb5f76a64
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-21 15:35:16 -0300

    feat: Interface ICar Criar interface genérica usando o zod, que extendendo a interface IVehicle.

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import { IVehicle } from './IVehicle';
+
+export const CarZodSchema = z.object({
+  doorsQty: z.number({
+    invalid_type_error: 'Doors Qty must be a number',
+  })
+    .int({
+      message: 'Doors Qty must be an integer',
+    })
+    .positive({
+      message: 'Doors Qty must have a positive value',
+    })
+    .min(2, {
+      message: 'Doors Qty must be 2 or more',
+    })
+    .max(4, {
+      message: 'Doors Qty must be 4 or less', 
+    }),
+
+  seatsQty: z.number({
+    invalid_type_error: 'Seats Qty must be a number',
+  })
+    .int({
+      message: 'Seats Qty must be an integer',
+    })
+    .min(2, {
+      message: 'Seats Qty must be 2 or more',
+    })
+    .max(7, {
+      message: 'Seats Qty must be 7 or less', 
+    }),
+});
+
+export type ICar = IVehicle & z.infer<typeof CarZodSchema>;
