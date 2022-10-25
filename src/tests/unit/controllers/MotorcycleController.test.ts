00a6dff92c1f56818ce38a2ce95b825a76d4e3a3
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-25 09:54:52 -0300

    test: Motrocycle Controller

--- src/tests/unit/controllers/MotorcycleController.test.ts
+++ src/tests/unit/controllers/MotorcycleController.test.ts
@@ -3,7 +3,9 @@ import chai from 'chai';
 import MotorcycleService from '../../../services/MotorcycleService';
 import MotorcycleController from '../../../controllers/MotorcycleController';
 import { Request, Response } from 'express';
-import { allMotorsMock, motorcycleMockwithId } from '../../mocks/motorcycleMocks';
+import { allMotorsMock, motorcycleMock, motorcycleMockwithId } from '../../mocks/motorcycleMocks';
+
+const { expect } = chai;
 
 describe('Motorcycle controller', () => {
   const service = new MotorcycleService();
@@ -22,5 +24,71 @@ describe('Motorcycle controller', () => {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     res.end = sinon.stub().returns(res);
-  })
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('Create a new motorcycle', () => {
+    it('With success', async () => {
+      req.body = motorcycleMock;
+      await controller.create(req, res);
+      const status = res.status as sinon.SinonStub;
+      const json = res.json as sinon.SinonStub;
+
+      expect(status.calledWith(201)).to.be.true;
+      expect(json.calledWith(motorcycleMockwithId)).to.be.true;
+    });
+  });
+
+  describe('Read all motorcycles', () => {
+    it('With success', async () => {
+      await controller.read(req, res);
+      const status = res.status as sinon.SinonStub;
+      const json = res.json as sinon.SinonStub;
+
+      expect(status.calledWith(200)).to.be.true;
+      expect(json.calledWith(allMotorsMock)).to.be.true;
+    });
+  });
+
+  describe('Read one motorcycle', () => {
+    it('With success', async () => {
+      req.params = { id: 'valid' };
+      await controller.readOne(req, res);
+      const status = res.status as sinon.SinonStub;
+      const json = res.json as sinon.SinonStub;
+
+      expect(status.calledWith(200)).to.be.true;
+      expect(json.calledWith(motorcycleMockwithId)).to.be.true;
+    });
+  });
+
+  describe('Update a motrocycle', () => {
+    it('With success', async () => {
+      req.params = { id: 'valid' };
+      req.body = motorcycleMock;
+      await controller.update(req, res);
+      
+      const status = res.status as sinon.SinonStub;
+      const json = res.json as sinon.SinonStub;
+
+      expect(status.calledWith(200)).to.be.true;
+      expect(json.calledWith(motorcycleMockwithId)).to.be.true;
+    });
+  });
+
+  describe('Delete a motrocycle', () => {
+    it('With success', async () => {
+      req.params = { id: 'valid' };
+      await controller.delete(req, res);
+
+      const status = res.status as sinon.SinonStub;
+      const end = res.end as sinon.SinonStub;
+
+      expect(status.calledWith(204)).to.be.true;
+      expect(end.calledWith()).to.be.true;
+    });
+  });
 });
